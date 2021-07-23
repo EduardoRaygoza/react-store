e26d822a8a4e22c05d164d86ef5eda3f59f3656a
Eduardo Raygoza <eduardoraygoza@gmail.com>
2021-07-23 17:27:43 -0500

    Implemented the Checkout container view and style

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
-const Checkout = () => <h1>Checkout</h1>;
+import '../styles/components/Checkout.scss';
+
+const Checkout = () => 
+  <div className="checkout">
+    <div className="checkout-content">
+      <h3>Lista de pedidos</h3>
+      <div className="checkout-item">
+        <div className="checkout-element">
+          <h4>ITEM_NAME</h4>
+          <span>$XXX</span>
+        </div>
+        <button type="button">Eliminar</button>
+      </div>
+    </div>
+    <div className="checkout-sidebar">
+      <h3>Precio total: $XXX</h3>
+      <button type="button">Continuar pedido</button>
+    </div>
+  </div>
 
 export default Checkout;
