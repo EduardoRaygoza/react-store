d34c4d6c6e68b2568256daa84149d5ffb15b294e
Eduardo Raygoza <eduardoraygoza@gmail.com>
2021-07-26 19:25:53 -0500

    Modified webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -51,18 +51,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: './assets/[name].css'
     }),
-    new Dotenv({
-      path: './.env',
-      safe:true,
-      systemvars:true,
-      defaults:false
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        CLIENT_ID_PP: JSON.stringify(process.env.CLIENT_ID_PP),
-        CLIENT_ID_GM: JSON.stringify(process.env.CLIENT_ID_GM)
-      }
-    })
+    new Dotenv()
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
